c15a824d295a26f411c8dcd7e365db548ad8eff7
fuxingjun <fuxingjun@qq.com>
2019-02-25 16:17:38 +0800

    打包公共代码配置

--- src/entry/index.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-ReactDOM.render(
-    <div>111</div>,
-    document.getElementById('root')
-);
--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -53,6 +53,27 @@ const prodConfig = {
             }
         ],
     },
+    optimization: {
+        splitChunks: {
+            cacheGroups: {
+                // 注意: priority属性
+                // 其次: 打包业务中公共代码
+                common: {
+                    name: "common",
+                    chunks: "all",
+                    minSize: 1,
+                    priority: 0
+                },
+                // 首先: 打包node_modules中的文件
+                vendor: {
+                    name: "vendor",
+                    test: /[\\/]node_modules[\\/]/,
+                    chunks: "all",
+                    priority: 10
+                }
+            }
+        }
+    },
     plugins: [
         new optimizeCss(),
         new MiniCssExtractPlugin({
